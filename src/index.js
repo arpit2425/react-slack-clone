9d5214c58d1d78d3000400508326e1562499eaab
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-07-11 22:40:30 +0530

    Format code

--- src/index.js
+++ src/index.js
@@ -1,42 +1,43 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './Components/App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  useHistory,
+} from 'react-router-dom';
 import Login from './Components/Auth/Login';
 import Register from './Components/Auth/Register';
-import firebase from './firebase'
+import firebase from './firebase';
 
-const Root = (props)=>{
-const histroy=useHistory();
-useEffect(() => {
-  firebase.auth().onAuthStateChanged(user=>{
-  if(user)
-  {
-  histroy.push("/");
-  }
-  })
- 
-}, )
+const Root = (props) => {
+  const histroy = useHistory();
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        histroy.push('/');
+      }
+    });
+  });
 
-return (
-
-        <Switch>
-            <Route exact path='/' component={App} />
-            <Route path='/login' component={Login} />
-            <Route path='/register' component={Register} />
-        </Switch>
-
-)
-}
-
-
+  return (
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Switch>
+  );
+};
 
 ReactDOM.render(
-       <Router><Root/></Router>
-    , document.getElementById('root'),
+  <Router>
+    <Root />
+  </Router>,
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
